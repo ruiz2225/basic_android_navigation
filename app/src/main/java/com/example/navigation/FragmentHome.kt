98c3338a9acb63355f5996d22c4258151dc47d2c
julio.ruiz <julio.ruiz@pragma.com.co>
2023-03-07 15:41:20 -0500

    Se agrega nuevo fragmento para ejemplo de menú en la parte superior los 3 puntos.

--- app/src/main/java/com/example/navigation/FragmentHome.kt
+++ app/src/main/java/com/example/navigation/FragmentHome.kt
@@ -1,12 +1,14 @@
 package com.example.navigation
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.navigation.databinding.FragmentHomeBinding
 
 
@@ -28,7 +30,18 @@ class FragmentHome : Fragment() {
         binding.btnPlay.setOnClickListener {
             it.findNavController().navigate(R.id.action_fragmentHome_to_fragmentQuestions)
         }
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider{
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.options_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return NavigationUI.onNavDestinationSelected(menuItem, requireView().findNavController())
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         return binding.root
     }
-
 }
